2e69fa1a17b975826ae691722211cd111e57634b
nsmotritski <nsmotritski@gmail.com>
2016-05-23 15:43:12 +0300

    Updated ELements class

--- src/main/java/issoft/training/java/Elements.java
+++ src/main/java/issoft/training/java/Elements.java
@@ -7,11 +7,6 @@ class Elements {
         elements = new Element[elementsNumber];
     }
 
-    Elements(String number) {
-        int elementsNumber = Integer.valueOf(number);
-        elements = new ElementNumber[elementsNumber];
-    }
-
 void printQueue () {
         for (Printable e: elements) {   //using interface link for elements
             e.print();
